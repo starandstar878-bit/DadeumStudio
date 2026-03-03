6b73f4f4ab2c4afdaa910613b2d61296cb3aa291
STAR <starandstar878-bit@users.noreply.github.com>
2026-03-03 21:42:50 +0900

    chore(build): switch build_check to VS18 and VisualStudio2026

--- build_check.bat
+++ build_check.bat
@@ -1,6 +1,23 @@
 @echo off
 setlocal
-set MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe
-"%MSBUILD%" "Builds\VisualStudio2022\DadeumStudio.sln" /t:Build /p:Configuration=Debug /p:Platform=x64 /m /nologo /v:minimal /flp:logfile=build_log.txt;verbosity=minimal
+
+set "MSBUILD="
+set "MSBUILD_VS18=C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
+set "MSBUILD_VS2022=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
+
+if exist "%MSBUILD_VS18%" set "MSBUILD=%MSBUILD_VS18%"
+if not defined MSBUILD if exist "%MSBUILD_VS2022%" set "MSBUILD=%MSBUILD_VS2022%"
+
+if not defined MSBUILD (
+  echo MSBuild not found.
+  echo EXIT_CODE=1 >> build_log.txt
+  endlocal
+  exit /b 1
+)
+
+set "SOLUTION=Builds\VisualStudio2026\DadeumStudio.sln"
+if not exist "%SOLUTION%" set "SOLUTION=Builds\VisualStudio2022\DadeumStudio.sln"
+
+"%MSBUILD%" "%SOLUTION%" /t:Build /p:Configuration=Debug /p:Platform=x64 /m /nologo /v:minimal /flp:logfile=build_log.txt;verbosity=minimal
 echo EXIT_CODE=%ERRORLEVEL% >> build_log.txt
 endlocal
